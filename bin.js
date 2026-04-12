849c74ad6addfead50304be6aab1fabb7a04499f
Eric Cornelissen <ericornelissen@gmail.com>
2026-04-12 19:14:42 +0200

    Correct CLI

--- bin.js
+++ bin.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 // SPDX-License-Identifier: Apache-2.0
 
 import * as console from "node:console";
@@ -8,10 +10,10 @@ Object.freeze(Object.prototype);
 Object.freeze(Array.prototype);
 Object.freeze(globalThis);
 
-const { stripFiles } = await import("./lib.js");
+const { stripFilesDirectives } = await import("./lib.js");
 try {
-	const files = argv.slice(2);
-	await stripFiles(files);
+	const files = argv[0].endsWith("node") ? argv.slice(2) : argv.slice(1);
+	await stripFilesDirectives(files);
 	exit(0);
 } catch (error) {
 	console.error(error);
