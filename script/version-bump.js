f26dd2d809dbf3beb4db82d3f0d75dccf5c2f5ca
Eric Cornelissen <ericornelissen@gmail.com>
2026-07-04 14:45:41 +0200

    Create command to perform version bump
    Simplify the release process, and in particular avoid forgetting to
    update the man page (like I did for v1.0.12).

---
+++ script/version-bump.js
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: Apache-2.0
+
+import { execSync } from "node:child_process";
+import { readFile, writeFile } from "node:fs/promises";
+import { argv, exit } from "node:process";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { inc } from "semver";
+
+if (argv[0].endsWith("/node")) argv.shift();
+
+const type = argv[1];
+if (!["major", "minor", "patch"].includes(type)) {
+	console.log("usage: `npm run version-bump <patch|minor|major>`");
+	exit(1);
+}
+
+const currentBranch = execSync("git branch --show-current").toString().trim();
+if (currentBranch !== "main") {
+	console.log("must be run from the default branch, 'main'");
+	exit(1);
+}
+
+const status = execSync("git status --porcelain").toString().trim();
+if (status !== "") {
+	console.log("work space is dirty, commit or stash changes first");
+	exit(1);
+}
+
+const root = resolve(dirname(fileURLToPath(import.meta.url)), "..");
+const packageJson = resolve(root, "package.json");
+const packageLockJson = resolve(root, "package-lock.json");
+const manDotOne = resolve(root, "man.1");
+
+const manifest = JSON.parse(await readFile(packageJson));
+manifest.version = inc(manifest.version, type);
+await writeFile(packageJson, JSON.stringify(manifest, null, 2) + "\n");
+
+const lockfile = JSON.parse(await readFile(packageLockJson));
+lockfile.version = manifest.version;
+lockfile.packages[""].version = manifest.version;
+await writeFile(packageLockJson, JSON.stringify(lockfile, null, 2) + "\n");
+
+const manfile = await readFile(manDotOne, { encoding: "utf-8" });
+const lines = manfile.split("\n");
+const month = Temporal.Now.plainDateISO().toLocaleString("en-US", {
+	month: "long",
+});
+lines[0] = `.TH "strip-comments-js" "1" "${month} 2026" "v${manifest.version}" "User Commands"`;
+await writeFile(manDotOne, lines.join("\n"));
+
+execSync("git commit --all --message 'version bump'");
