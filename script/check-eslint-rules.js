3eebe8cfecbad4365229f6cdb16593887cd30280
Eric Cornelissen <ericornelissen@gmail.com>
2026-07-12 11:19:07 +0200

    Enforce all ESLint (plugin) rules are configured
    Based on github.com/ericcornelissen/shescape@9056303

---
+++ script/check-eslint-rules.js
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT-0
+
+import process from "node:process";
+
+const configModule = await import("../.eslint.js");
+const configArray = configModule.default;
+
+const all = new Set();
+const configured = new Set();
+
+for (const config of configArray) {
+	for (const pluginName in config.plugins) {
+		const plugin = config.plugins[pluginName];
+		for (const ruleName in plugin.rules) {
+			const rule = plugin.rules[ruleName];
+			if (!rule?.meta?.deprecated) {
+				const ruleId = pluginName ? `${pluginName}/${ruleName}` : ruleName;
+				all.add(ruleId);
+			}
+		}
+	}
+
+	for (const ruleId in config.rules) {
+		configured.add(ruleId);
+	}
+}
+
+const unconfigured = all.difference(configured);
+if (unconfigured.size > 0) {
+	console.log(`'${[...unconfigured].join("'\n'")}'`);
+	console.log("");
+	console.log(
+		unconfigured.size,
+		"missing rule(s) found.",
+		"Explicitly configure each of them.",
+	);
+
+	process.exit(1);
+} else {
+	console.log("No problems detected");
+}
