96f71fe793421606f6a1969ce86832068307446a
Eric Cornelissen <ericornelissen@gmail.com>
2026-08-08 18:30:22 +0200

    Fix stripping when regex is the dividend in a ctrl flow expression
    Relates to 11168f4f32e90c21a2d84e0a68038ddd4a4ac5de

--- main.js
+++ main.js
@@ -133,6 +133,12 @@ function $code(chars, result, options, match) {
 					if (next[0] === "/" && next[1] !== "/" && next[1] !== "*") {
 						result.push(chars.next());
 						if (!$regexp(chars, result)) return false;
+
+						while (whitespaceExpr.test(chars.peek())) result.push(chars.next());
+						const next = chars.peek(2);
+						if (next[0] === "/" && next[1] !== "/" && next[1] !== "*") {
+							result.push(chars.next());
+						}
 					}
 				}
 
